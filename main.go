0b2128c0c4f1f52fc09900a333bbf01005de29f0
kendo5731 <aur.baumann@gmail.com>
2016-05-20 14:23:07 +0200

    Creates a separate package for our HTTP handlers
    In most cases, a Go app is splited in multiple packages.
    Though in this case, the code base is still quite small, a new "api"
    package is created for our HTTP handlers in order to illustrate the
    principle.
    
    Basically in our workspace, each directory represents a package that
    contains one or many files.
    The "api" package sources are stored in the "api" sub-directory, and
    each source file have to describe "package api".
    
    Note that in "api/repos.go", the handler for "/hello" was moved and
    given the name "Hello". The capitalized name means that the function is
    exported from the package, thus package users will be able to use the
    "Hello" function. If we named the function "hello", then it would be
    unexported. This rule applies to everything in a package (functions,
    variables, types, etc...).
    
    In "main.go" on line 7, the "api" package is exported given its path
    relative to the $GOPATH/src folder, and "api.Hello" is now passed as
    the second argument of "http.HandleFunc".

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/aubm/github-aggregator/api"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, Go!"))
-	})
+	http.HandleFunc("/hello", api.Hello)
 	fmt.Println("Application started on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
